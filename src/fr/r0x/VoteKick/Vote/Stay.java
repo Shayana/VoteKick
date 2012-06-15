7c5b66fd25bf71b53bba00679cd34eb09029f2cd
Vianney Vienne <shayana@r0x.fr>
2012-06-16 00:30:40 +0200

    Done

---
+++ src/fr/r0x/VoteKick/Vote/Stay.java
@@ -0,0 +1,52 @@
+package fr.r0x.VoteKick.Vote;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+
+import fr.r0x.VoteKick.Main.Main;
+
+public class Stay extends Vote{
+
+	public Stay(Main plugin, Player voter, Player voted, String vote)
+	{
+		super(plugin, voter, voted, vote);
+	}
+
+	
+	public void stay()
+	{
+		if (plugin.kicks.containsKey(voted))
+		{
+			setMap(plugin.kicks);
+		}
+		else if(plugin.bans.containsKey(voted))
+		{
+			setMap(plugin.bans);
+		}
+		else if(plugin.tempbans.containsKey(voted))
+		{
+			setMap(plugin.tempbans);
+		}
+		else{
+			voter.sendMessage(plugin.msg.noVote(voted));
+			return;
+		}
+		if(canVote())
+		{
+			int votes = map.get(voted);
+			map.put(voted, votes - 1);
+			voters.put(voter, voted);
+			Bukkit.broadcastMessage(plugin.msg.remainingVotes(this));
+			for (Player p : Bukkit.getOnlinePlayers())
+			{
+				if(p.hasPermission("votekick.notify"))
+				{
+					p.sendMessage(plugin.msg.hasSupported(voter, voted));
+				}
+			}
+			
+		}
+	}
+
+	
+}
